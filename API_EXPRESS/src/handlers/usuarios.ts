93a5296220236a1e963bc62c554f3f44d4ce9b84
Matias Peñaloza <mpenalozaa@usm.cl>
2025-07-07 16:19:48 -0400

    BASE DE GATOS
    SE CREARON LOS ENDPOINTS Y LA CONEXION A LA BASE DE DATOS JUNTO A SU CREACION DE TABLAS

--- API_EXPRESS/src/handlers/usuarios.ts
+++ API_EXPRESS/src/handlers/usuarios.ts
@@ -1,5 +1,19 @@
 import {Request,Response} from "express"
 
-export const getUsuarios = async(request:Request,response:Response)=>{
+export const getUsuario = async(request:Request,response:Response)=>{
     response.json('Listar usuarios')
+}
+
+export const loginUsuario = async(request:Request,response:Response)=>{
+    response.json('Iniciar sesión de usuario')
+}
+
+export const cerrarSesionUsuario = async(request:Request,response:Response)=>{
+    response.json('Cerrar sesión de usuario')
+}
+export const registrarUsuario = async(request:Request,response:Response)=>{
+    response.json('Registrar nuevo usuario')
+}
+export const cambiarContrasena = async(request:Request,response:Response)=>{
+    response.json('Cambiar contraseña de usuario')
 }
--- API_EXPRESS/src/server.ts
+++ API_EXPRESS/src/server.ts
@@ -1,10 +1,26 @@
 import Express from 'express'
 import router from './router'
+import db from './config/database'
+
 const server = Express()
+
+//CONEXION A BD
+async function conectarBD(){
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('CONECTADOS!!!!!!')
+    } catch (error) {
+        console.log('Error al conectar a la base de datos:')
+        console.log(error)
+    }
+}
+
+conectarBD()
 
 
 //Todos los request que comiencen con /api se deben derivar a router.ts
-server.use('/api',router)
+server.use('/api', router)
 
 
 export default server
